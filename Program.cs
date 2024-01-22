75b4b4fe5ddfdb71b2eeb36c4e047f1cb8d90fd3
Maria Babina <babina.mariyka@gmail.com>
2024-01-22 10:43:41 +0200

    Create void PrintArray

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string[] startArray = { "Hello", "my", "teacher", "!", ":-)" };
+PrintArray(startArray);
+
+
+void PrintArray(string[] arr)
+{
+    string str = "[" + String.Join(", ", arr) + "]";
+    Console.WriteLine(str);
+}
