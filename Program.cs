75498a136df1ddf7001e6c15d42190c9792bff72
Maria Babina <babina.mariyka@gmail.com>
2024-01-22 10:44:51 +0200

    Create void CalculateArray

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
 ﻿string[] startArray = { "Hello", "my", "teacher", "!", ":-)" };
 PrintArray(startArray);
-
+string [] resultArray = CalculateArray(startArray);
 
 void PrintArray(string[] arr)
 {
     string str = "[" + String.Join(", ", arr) + "]";
     Console.WriteLine(str);
+}
+string[] CalculateArray(string[] arr)
+{
+    string[] resultArray = new string[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            Array.Resize(ref resultArray, resultArray.Length +1 );
+            resultArray [resultArray.Length-1] = arr [i];
+        }
+    }
+    return resultArray;
 }
